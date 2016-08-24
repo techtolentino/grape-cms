0dc08b90f7dbc28f95c27eb90d2105a24aa1fe7d
Randy Tolentino <makertolentino@gmail.com>
2016-08-24 03:47:35 -0500

    :page_facing_up: add form

--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,12 @@
-posts.js
+var express = require('express');
+var router = express.Router();
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
+
+router.get('/add', function(req, res, next){
+    res.render('addline', {
+        "title": 'New Line'
+    })
+});
+
+module.exports = router;
