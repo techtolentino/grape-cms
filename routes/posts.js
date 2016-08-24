f0bb7d3083de4f99be9d1f6e33b8bb4e5e98b434
Randy Tolentino <makertolentino@gmail.com>
2016-08-24 04:40:09 -0500

    :page_facing_up: create form up

--- routes/posts.js
+++ routes/posts.js
@@ -3,10 +3,53 @@ var router = express.Router();
 var mongo = require('mongodb');
 var db = require('monk')('localhost/nodeblog');
 
-router.get('/add', function(req, res, next){
-    res.render('addline', {
-        "title": 'New Line'
+router.get('/create', function(req, res, next){
+    var categories = db.get('categories');
+
+    categories.find({}, {}, function(err, categories){
+        res.render('create', {
+            "title": 'New Line',
+            "categories": categories
+        })
     })
+
+});
+
+router.post('/create', function(req, res, next) {
+    // get new line values
+    var title = req.body.title;
+    var category = req.body.category;
+    var body = req.body.body;
+    var date = new Date();
+
+    req.checkBody('title', 'Title is required').notEmpty();
+    req.checkBody('body', 'Body is required');
+
+    var errors = req.validationErrors();
+        if(errors) {
+            res.render('create', {
+                "errors": errors,
+                "title": title,
+                "body": body
+            });
+        } else {
+            var posts = db.get('posts');
+
+            posts.insert({
+                "title": title,
+                "body": body,
+                "category": category,
+                "date": date
+            }, function(err, post) {
+                if(err) {
+                    res.send('There was an issue submitting the new Line')
+                } else {
+                    req.flash('success', 'New Line submitted');
+                    res.location('/');
+                    res.redirect('/')
+                }
+            })
+        }
 });
 
 module.exports = router;
