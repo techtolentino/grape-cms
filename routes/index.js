aa5a814008af79730d7b0137886135a95babae3e
Randy Tolentino <makertolentino@gmail.com>
2016-08-26 04:20:46 -0500

    wire mongo client

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var mongo = require('mongodb');
-var db = require('monk')('localhost/nodeblog');
+var db = require('monk')('@ds015899.mlab.com:15899/lines');
 
 /* Get Lines */
 router.get('/', function(req, res, next) {
     var db = req.db;
-    var posts = db.get('posts');
+    var posts = db.collection('posts');
+
 
     posts.find({}, {}, function(err, posts) {
+    console.log(err, "<<<<<<<<<<<");
         res.render('index', {
             "posts": posts
         })
